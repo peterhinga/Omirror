2c011bed3292c473617a4c37d0b8ea37132aa1d4
Peter Hinga <114114142+peterhinga@users.noreply.github.com>
2025-01-21 13:19:24 +0300

    handle user input

---
+++ main.py
@@ -0,0 +1,9 @@
+#get a tor link
+#connect script to the dark web "onion network"
+
+#function for link
+
+def linkInput(link):
+    userInput = input("Hey enter the onion link:")
+
+linkInput(userInput)
